ab1b083140a6d52e66625b79f1ea40929a402900
Jannik H. Orzek <orzek@mpib-berlin.mpg.de>
2022-07-11 11:08:31 +0200

    added noDotDotDot function to capture ...

---
+++ tests/testthat/test-noDotDotDot.R
@@ -0,0 +1,36 @@
+test_that("testing noDotDotDot", {
+  library(lessSEM)
+  
+  # test function that only takes one argument
+  fn <- function(params){
+    return(params)
+  }
+  
+  out <- try(
+    lessSEM::noDotDotDot(fn = fn, x = 1, y = "a", z = 1)
+  )
+  
+  testthat::expect_equal(is(out, "list"), TRUE)
+  
+  # try calling the function
+  
+  testthat::expect_equal(out$fn(1, "a", additionalArguments = out$additionalArguments) == 1, 
+                         c(a = TRUE))
+  
+  # test function that takes additional arguments
+  fn <- function(params, x, y, z){
+    return(params)
+  }
+  
+  out <- try(
+    lessSEM::noDotDotDot(fn = fn, x = 1, y = "a", z = 1)
+  )
+  
+  testthat::expect_equal(is(out, "list"), TRUE)
+  
+  # try calling the function
+  
+  testthat::expect_equal(out$fn(1, "a", additionalArguments = out$additionalArguments) == 1, 
+                         c(a = TRUE))
+  
+})
